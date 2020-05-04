b19d10581e1fd770a0b1168cfbc0e0414423ff3b
Courtskit <Courtskit@gmail.com>
2020-05-04 15:11:14 -0700

    created method customers per hour

--- js/app.js
+++ js/app.js
@@ -15,57 +15,69 @@ var seattle = {
 };
 console.log(seattle.customersPerHour());
 
-// var tokyo = {
-//   minCustomer: 3,
-//   maxCustomer: 24,
-//   averageCookieSale: 1.2,
-//   customersPerHour: function () {
-
-//   },
-//   cookiesPerHour: function () {
-//   }
-// };
-
-
-
-// var dubai = {
-//   minCustomer: 11,
-//   maxCustomer: 38,
-//   averageCookieSale: 3.7,
-//   customersPerHour: function () {
-
-//   },
-//   cookiesPerHour: function () {
-
-//   }
-// };
+var tokyo = {
+  minCustomer: 3,
+  maxCustomer: 24,
+  averageCookieSale: 1.2,
+  customersPerHour: function () {
+    var min = this.minCustomer;
+    var max = this.maxCustomer;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  // cookiesPerHour: function () {
+  // }
+};
+console.log(tokyo.customersPerHour());
 
 
+var dubai = {
+  minCustomer: 11,
+  maxCustomer: 38,
+  averageCookieSale: 3.7,
+  customersPerHour: function () {
+    var min = this.minCustomer;
+    var max = this.maxCustomer;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  // cookiesPerHour: function () {
 
-
-// var paris = {
-//   minCustomer: 20,
-//   maxCustomer: 38,
-//   averageCookieSale: 2.3,
-//   customersPerHour: function () {
-
-//   },
-//   cookiesPerHour: function () {
-
-//   }
-// };
+  // }
+};
+console.log(dubai.customersPerHour());
 
 
 
 
-// var lima = {
-//   minCustomer: 2,
-//   maxCustomer: 16,
-//   averageCookieSale: 4.6,
-//   customersPerHour: function () {
+var paris = {
+  minCustomer: 20,
+  maxCustomer: 38,
+  averageCookieSale: 2.3,
+  customersPerHour: function () {
+    var min = this.minCustomer;
+    var max = this.maxCustomer;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  // cookiesPerHour: function () {
 
-//   },
-//   cookiesPerHour: function () {
+  // }
+};
 
-//   }
-// };
+console.log(paris.customersPerHour());
+
+
+
+
+var lima = {
+  minCustomer: 2,
+  maxCustomer: 16,
+  averageCookieSale: 4.6,
+  customersPerHour: function () {
+    var min = this.minCustomer;
+    var max = this.maxCustomer;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  // cookiesPerHour: function () {
+
+  // }
+};
+console.log(lima.customersPerHour());
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+  // cookiesPerHour: function () {
+
+  // }
